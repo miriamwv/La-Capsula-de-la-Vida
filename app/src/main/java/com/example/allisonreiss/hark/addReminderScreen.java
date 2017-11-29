88a0d9c96ff6f9cacaf81bf16917b122d43be43a
Veronica Lin <velin@ucsd.edu>
2017-11-28 22:14:59 -0800

    Added reminders pages

---
+++ app/src/main/java/com/example/allisonreiss/hark/addReminderScreen.java
@@ -0,0 +1,17 @@
+package com.example.allisonreiss.hark;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+/**
+ * Created by veronica.lin1218 on 11/28/2017.
+ */
+
+public class addReminderScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_reminders_screen);
+    }
+}
---
+++ app/src/main/java/com/example/allisonreiss/hark/reminderSettingsScreen.java
@@ -0,0 +1,16 @@
+package com.example.allisonreiss.hark;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by veronica.lin1218 on 11/28/2017.
+ */
+
+public class reminderSettingsScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.reminder_settings_screen);
+    }
+}
