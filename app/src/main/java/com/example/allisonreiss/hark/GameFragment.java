64cb19fe920d95a50a3f32e9f2a00a54029f77ae
Allison Reiss <allisonreiss1@gmail.com>
2017-11-30 15:21:09 -0800

    Implemented BottomNav and Fragment screens

---
+++ app/src/main/java/com/example/allisonreiss/hark/GameFragment.java
@@ -0,0 +1,29 @@
+package com.example.allisonreiss.hark;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class GameFragment extends Fragment {
+
+    public static GameFragment newInstance() {
+        GameFragment fragment = new GameFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_game, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/allisonreiss/hark/ReminderFragment.java
@@ -0,0 +1,29 @@
+package com.example.allisonreiss.hark;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ReminderFragment extends Fragment {
+
+    public static ReminderFragment newInstance() {
+        ReminderFragment fragment = new ReminderFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_reminder, container, false);
+    }
+}
