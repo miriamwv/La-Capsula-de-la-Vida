3f27530e3a6d50c09bdcc99c82f97f7007a16e3b
Allison Reiss <allisonreiss1@gmail.com>
2017-11-30 18:36:14 -0800

    Added Calendar Screen

---
+++ app/src/main/java/com/example/allisonreiss/hark/CalendarScreen.java
@@ -0,0 +1,27 @@
+package com.example.allisonreiss.hark;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.Toast;
+
+public class CalendarScreen extends AppCompatActivity {
+
+    CalendarView calendar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar_screen);
+/*
+        calendar = (CalendarView) findViewById(R.id.calendar);
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                Toast.makeText(getApplicationContext(), dayOfMonth + '/' + month + '/' + year, Toast.LENGTH_LONG);
+            }
+        });
+        */
+    }
+}
--- app/src/main/java/com/example/allisonreiss/hark/ProfileFragment.java
+++ app/src/main/java/com/example/allisonreiss/hark/ProfileFragment.java
@@ -1,12 +1,17 @@
 package com.example.allisonreiss.hark;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ProfileFragment extends Fragment {
+
+    private Button calendarBtn;
 
     public static ProfileFragment newInstance() {
         ProfileFragment fragment = new ProfileFragment();
@@ -22,6 +27,15 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        calendarBtn = (Button) view.findViewById(R.id.calendarButton);
+        calendarBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), CalendarScreen.class);
+                startActivity(intent);
+            }
+        });
         return view;
     }
 }
