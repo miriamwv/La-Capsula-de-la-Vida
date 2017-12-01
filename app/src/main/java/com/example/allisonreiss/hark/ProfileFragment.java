ffff1993f8f8976c7c0bcb79a31c23fef38005ad
Allison Reiss <allisonreiss1@gmail.com>
2017-11-30 17:58:36 -0800

    Created profile layout

--- app/src/main/java/com/example/allisonreiss/hark/ProfileFragment.java
+++ app/src/main/java/com/example/allisonreiss/hark/ProfileFragment.java
@@ -21,6 +21,7 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        return view;
     }
 }
