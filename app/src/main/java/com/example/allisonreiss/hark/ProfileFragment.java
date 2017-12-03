b450c57b4d2ada04a1bc1cf7f70e877eb655ce91
Allison Reiss <allisonreiss1@gmail.com>
2017-12-02 16:59:15 -0800

    Trying to get Calendar fragments to communicate

--- app/src/main/java/com/example/allisonreiss/hark/ProfileFragment.java
+++ app/src/main/java/com/example/allisonreiss/hark/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.example.allisonreiss.hark;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -38,4 +39,34 @@ public class ProfileFragment extends Fragment {
         });
         return view;
     }
+
+/*
+    // Set up profile selected listener
+    OnProfileSelectedListener mCallback;
+
+    // Container Activity must implement this interface
+    public interface OnProfileSelectedListener {
+        public void onArticleSelected(int position);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        // This makes sure that the container activity has implemented
+        // the callback interface. If not, it throws an exception
+        try {
+            mCallback = (OnProfileSelectedListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnHeadlineSelectedListener");
+        }
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        // Send the event to the host activity
+        mCallback.onArticleSelected(position);
+    }
+*/
 }
