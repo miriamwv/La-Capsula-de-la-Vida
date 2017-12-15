3adabb941cc66eccea383e43ae9277f7f4bd0cde
Allison Reiss <allisonreiss1@gmail.com>
2017-12-14 17:49:16 -0800

    Story now goes to home screen

--- app/src/main/java/com/example/allisonreiss/hark/fourthStory.java
+++ app/src/main/java/com/example/allisonreiss/hark/fourthStory.java
@@ -1,6 +1,7 @@
 package com.example.allisonreiss.hark;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -56,8 +57,8 @@ public class fourthStory extends Fragment {
         nextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Fragment frag = HomeFragment.newInstance();
-                replaceFragment(frag);
+                Intent i = new Intent(getActivity(), HomeScreen.class);
+                getActivity().startActivity(i);
             }
         });
 
